081e4f024befbaae75ffad6be6c5267a2c98acda
mhibbert <hibbert.michael@gmail.com>
2020-08-19 09:27:07 +0100

    updated calculator

--- src/ui/SettingsDialog.js
+++ src/ui/SettingsDialog.js
@@ -1,5 +1,23 @@
+const { QMainWindow, QLabel, WidgetEventTypes} = require("@nodegui/nodegui");
+
+
+
 const openSettingsDialog = () => {
-    console.log("Settings Dialog Opened")
+    console.log("Settings Dialog Opened");
+
+    const win = new QMainWindow();
+    win.setWindowTitle("Evermore Settings");
+
+    const label = new QLabel(win);
+    label.setText("Root Folder: Root Folder:Root Folder:Root Folder:Root Folder:Root Folder:Root Folder:");
+
+    win.addEventListener(WidgetEventTypes.Close, function(e) {
+        return null;
+    });
+
+    win.show();
+
+    global.win = win;
 }
 
 export default openSettingsDialog;
